8e9f226de2fbaaa67a91762c5e977ada3f7ce7a5
Tre Gonzales <Tre@education.com>
2018-12-04 22:51:43 -0800

    initial rough demo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public GameObject pauseMenu;
+	public bool paused;
+	public bool mainMenu;
+    public GameObject user;
+	// Use this for initialization
+	void Awake () {
+        if (instance == null) {
+            instance = this;
+        }
+        if (!mainMenu) {
+			paused = false;
+			Time.timeScale = 1.0f;
+			pauseMenu.SetActive(paused);
+            Cursor.lockState = CursorLockMode.Locked;
+		}
+	}
+
+    public void RestartTheGameAfterSeconds(float seconds){
+		Time.timeScale = 1.0f;
+		StartCoroutine (LoadSceneAfterSeconds (seconds, SceneManager.GetActiveScene ().name));
+	}
+
+	public void LoadScene(float seconds, string sceneName){
+		StartCoroutine (LoadSceneAfterSeconds (seconds, sceneName));
+	}
+
+	public void LoadSceneByIndex(int i){
+		SceneManager.LoadScene(i);
+	}
+
+	public void LoadMainMenu() {
+		SceneManager.LoadScene("startMenu");
+	}
+
+    public void LoadProject() {
+        SceneManager.LoadScene("project");
+    }
+
+	public void LoadNextScene(float seconds) {
+		StartCoroutine (LoadSceneAfterSeconds (seconds, null));
+	}
+
+	IEnumerator LoadSceneAfterSeconds(float seconds, string sceneName){
+		yield return new WaitForSeconds (seconds);
+		if (sceneName == null) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+		else {
+			SceneManager.LoadScene (sceneName);
+		}
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape) && !mainMenu) {
+			TogglePauseMenu();
+		}
+
+        // Debug.Log(Cursor.lockState);
+        if (!mainMenu && !paused && Cursor.lockState == CursorLockMode.None) {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+		
+		// if (Input.GetKeyDown(KeyCode.R) && !mainMenu) {
+        // 	RestartTheGameAfterSeconds(0.5f);
+    	// }
+	}
+
+	public void TogglePauseMenu() {
+		if (paused)
+        {
+            user.GetComponent<MouseLook>().enabled = true;
+            Camera.main.GetComponent<MouseLook>().enabled = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            pauseMenu.SetActive(!paused);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            user.GetComponent<MouseLook>().enabled = false;
+            Camera.main.GetComponent<MouseLook>().enabled = false;
+            pauseMenu.SetActive(!paused);
+            Time.timeScale = 0f;
+        }
+        paused = !paused;
+    }
+}
---
+++ Assets/Scripts/userController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userController : MonoBehaviour {
+
+    public float walkSpeed;
+
+    public float walkBounce;
+
+    Transform camTrans;
+
+    RaycastHit hit;
+
+    Rigidbody body;
+
+    bool moving;
+
+    public float bounceTime;
+
+    float timer;
+
+    // Use this for initialization
+    void Start () {
+        // Cursor.lockState = CursorLockMode.Locked; 
+		camTrans = Camera.main.transform;
+        body = GetComponent<Rigidbody>();
+        moving = false;
+        timer = bounceTime;
+	}
+
+    //to do:
+    //create pause menu and deal with showing and hiding cursor
+	
+	// Update is called once per frame
+	void Update () {
+        moving = false;
+        if (Input.GetKey(KeyCode.W)) {
+            body.velocity = Vector3.ClampMagnitude(body.velocity + transform.forward, walkSpeed);
+            moving = true;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+           body.velocity = Vector3.ClampMagnitude(body.velocity + transform.right, walkSpeed);
+            moving = true;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+           body.velocity = Vector3.ClampMagnitude(body.velocity + -1*transform.right, walkSpeed);
+            moving = true;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            body.velocity = Vector3.ClampMagnitude(body.velocity + -1*transform.forward, walkSpeed);
+            moving = true;
+        }
+        if (moving) {
+            timer -= Time.deltaTime;
+            if (timer < 0) {
+                Debug.Log("bounce");
+                body.velocity += Vector3.up*walkBounce;
+                timer = bounceTime;
+            }
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(camTrans.position, camTrans.forward, out hit, 500)) {
+                if (hit.transform.CompareTag("audio")) {
+                    hit.transform.gameObject.GetComponent<AudioSource>().Play();
+                }
+            }
+        }
+		
+	}
+}
